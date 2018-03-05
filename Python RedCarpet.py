f29e1e7a2f1d2cc86545d6fc44d9ee267f1a03d6
Kachra_Gumnam <g.pandey2990@gmail.com>
2018-03-05 11:31:52 +0530

    Update Python RedCarpet.py

--- Python RedCarpet.py
+++ Python RedCarpet.py
@@ -1,12 +1,12 @@
-import json
-import os
-import csv
+#import json
+#import os
+#import csv
 
-path_redCarpet = 'C:/Users/gaurav_pandey/Downloads/Softwares/Data File/RedCarpet/socialmediadata-tweets-' \
+#path_redCarpet = 'C:/Users/gaurav_pandey/Downloads/Softwares/Data File/RedCarpet/socialmediadata-tweets-' \
                  'of-congress-november/socialmediadata-tweets-of-congress-november'
 
-os.chdir(path_redCarpet)
-print os.getcwd();
+#os.chdir(path_redCarpet)
+#print os.getcwd();
 
 # files=[f for f in os.listdir('.') if os.path.isfile(f)]
 # print(files)
@@ -26,9 +26,9 @@ print os.getcwd();
 # json_dictionary={"ham":"YES","egg":"YES","yolk":"NO"}
 # print(json_dictionary["egg"])
 
- redCarpet_data_1ist = json.load(open('2017-11-01.json'))
- redCarpet_data_1ist = redCarpet_data_1ist + json.load(open('2017-11-02.json'))
- redCarpet_data_1ist = redCarpet_data_1ist + json.load(open('2017-11-03.json'))
+# redCarpet_data_1ist = json.load(open('2017-11-01.json'))
+# redCarpet_data_1ist = redCarpet_data_1ist + json.load(open('2017-11-02.json'))
+# redCarpet_data_1ist = redCarpet_data_1ist + json.load(open('2017-11-03.json'))
 
 # for i in redCarpet_data_1ist:
 #    for key, value in i.iteritems():
@@ -36,10 +36,34 @@ print os.getcwd();
 # print(type(redCarpet_data_1ist))
 
 
-for i in redCarpet_data_1ist:
-    print(i)
+#for i in redCarpet_data_1ist:
+#    print(i)
 
 # Metrics involved in the data: ID, ScreenName, User_ID, Time, Link, Text, Source
 # with open("consolidated_RedCarpet.csv",'wb') as main_file:
 #    write = csv.writer(main_file,quoting=csv.QUOTE_NONE)
 #    write.writerow(redCarpet_data_1ist)
+
+#03-05-2018
+import json
+import os
+import csv
+import pandas as pd
+
+path_redCarpet = 'C:/Users/gaurav_pandey/Downloads/Softwares/Data File/RedCarpet/socialmediadata-tweets-' \
+                 'of-congress-november/socialmediadata-tweets-of-congress-november'
+
+os.chdir(path_redCarpet)
+print os.getcwd();
+
+redCarpet_data_1ist = json.load(open('2017-11-01.json'))
+redCarpet_data_1ist = redCarpet_data_1ist + json.load(open('2017-11-02.json'))
+redCarpet_data_1ist = redCarpet_data_1ist + json.load(open('2017-11-03.json'))
+
+final_redCarpet = pd.DataFrame.from_records(redCarpet_data_1ist)
+
+final_redCarpet.to_csv("consolidated_RedCarpet.csv",sep=",",encoding='utf-8')
+
+
+
+
